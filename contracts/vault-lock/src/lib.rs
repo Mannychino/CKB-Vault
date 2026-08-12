8ef38f675fca5f1a46ae8f72b3a9cb7375c8f912
victor Osinachi <victorosinachi26@gmail.com>
2026-08-13 00:47:28 +1200

    New changes to the project

--- contracts/vault-lock/src/lib.rs
+++ contracts/vault-lock/src/lib.rs
@@ -1,11 +1,10 @@
 #![cfg_attr(not(feature = "library"), no_std)]
-#![allow(special_module_name)]
-#![allow(unused_attributes)]
 
 #[cfg(feature = "library")]
-mod main;
-
-#[cfg(feature = "library")]
-pub use main::program_entry;
-
 extern crate alloc;
+
+#[cfg(feature = "library")]
+mod contract;
+
+#[cfg(feature = "library")]
+pub use contract::program_entry;
