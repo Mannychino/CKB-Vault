b28f0fab008bfb4579540997ffb938eb408ad4b9
victor Osinachi <victorosinachi26@gmail.com>
2026-08-25 15:46:31 +1200

    continued with the vault lifecycle and test

---
+++ contracts/vault-lock/scripts/create_vault.ts
@@ -0,0 +1,17 @@
+import { ccc } from "@ckb-ccc/core";
+
+async function main() {
+  const client = new ccc.ClientPublicTestnet(
+    "http://127.0.0.1:8114"
+  );
+
+  const tip = await client.getTip();
+
+  console.log("Connected to CKB devnet!");
+  console.log("Tip block:", tip.toString());
+}
+
+main().catch((error) => {
+  console.error("Failed to connect:", error);
+  process.exit(1);
+});
